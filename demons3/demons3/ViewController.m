e153cca55f92694874d4e4e3d970c99aa30d2c5c
tarena1002 <tarena1002@huangyan.local>
2015-10-08 12:32:35 +0800

    sfks

---
+++ demons3/demons3/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  demons3
+//
+//  Created by tarena1002 on 15/10/8.
+//  Copyright (c) 2015年 hy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"lsjf");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
